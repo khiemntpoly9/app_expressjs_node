616706fa041f619650d78a5d439e8e046a092981
Trung Khiem <trungkhiem1412@gmail.com>
2022-11-04 10:49:59 +0700

    kkk

--- app/config/connectDB.js
+++ app/config/connectDB.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'labnodejs',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 });
 
 db.connect((err) => {
